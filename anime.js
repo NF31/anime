658099a8a999c1db664cc8026e493fd827eab1a8
Jean-Eudes Woto <107761073+NF31@users.noreply.github.com>
2023-02-25 05:07:51 +0100

    Add files via upload

---
+++ anime.js
@@ -0,0 +1,9 @@
+// menu responsive 
+
+const kaleidoscope = document.querySelector('.kaleidoscope')
+const menu = document.querySelector(".menu")
+
+kaleidoscope.addEventListener('click',() =>
+{
+ menu.classList.toggle('mobile-menu')
+});
